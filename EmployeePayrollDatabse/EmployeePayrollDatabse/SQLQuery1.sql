4150c59b4763b56940190b171f17d0d6b9cb5917
Prachi <prachikale136@gmail.com>
2023-04-28 00:40:31 +0530

    [Prachi] Add: create a table

--- EmployeePayrollDatabse/EmployeePayrollDatabse/SQLQuery1.sql
+++ EmployeePayrollDatabse/EmployeePayrollDatabse/SQLQuery1.sql
@@ -1,3 +1,13 @@
 ﻿create database Employee_Payroll
 
 use Employee_Payroll
+
+create table Employee_PayrollTable
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start_date date not null
+)
+
+select * from Employee_PayrollTable

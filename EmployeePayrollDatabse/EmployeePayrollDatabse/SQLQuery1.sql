8dbf2abb7d69ba91e183418a81862c0797da0334
Prachi <prachikale136@gmail.com>
2023-04-28 01:02:02 +0530

    [Prachi ] Add: retrive data from table

--- EmployeePayrollDatabse/EmployeePayrollDatabse/SQLQuery1.sql
+++ EmployeePayrollDatabse/EmployeePayrollDatabse/SQLQuery1.sql
@@ -16,5 +16,6 @@ insert into Employee_PayrollTable values ('Prachi',100000.0, '2018-3-12')
 insert into Employee_PayrollTable values('terissa',20000.0,'2020-5-21'),
 ('prajakta',50000.0,'2019-11-13')
 
-select * from Employee_PayrollTable
 
+select * from Employee_PayrollTable
+select name from Employee_PayrollTable
